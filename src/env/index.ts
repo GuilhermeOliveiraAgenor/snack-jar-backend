007394d37aca3d314a6bb853be6d1b0fe0e341c0
Guilherme <guilhermeagenor1012@gmail.com>
2025-10-25 20:03:01 -0300

    feature: folder structure

---
+++ src/env/index.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config'
+import {z} from 'zod'
+
+const envSchema = z.object({//table with variable
+    NODE_ENV: z.enum(['dev','test','production']).default('dev'),
+    PORT: z.coerce.number().default(3000),
+    JWT_SECRET: z.string()
+})
+
+const _env = envSchema.safeParse(process.env)//validate variable
+
+if(!_env.success == true){
+    console.log('Invalid variable', _env.error)
+    throw new Error('Error in variables')
+}
+
+export const env = _env.data
+
+
