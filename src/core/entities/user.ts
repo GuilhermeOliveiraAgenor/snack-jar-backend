57f03b99d39dc8296aec6f092477a6a77c49362a
Guilherme <guilhermeagenor1012@gmail.com>
2025-12-06 19:37:06 -0300

    feature: add user entity

---
+++ src/core/entities/user.ts
@@ -0,0 +1,66 @@
+export interface UserProps {
+  // create interface
+  name: string;
+  email: string;
+  password: string;
+  createdAt: Date | null;
+  updatedAt: Date | null;
+}
+
+export class User {
+  constructor(
+    private readonly _id: string, // create id
+    private props: UserProps, // import fields props
+  ) {
+    this.props.createdAt = props.createdAt ?? new Date(); /// optional fields
+    this.props.updatedAt = props.updatedAt ?? null;
+  }
+  get name() {
+    return this.props.name;
+  }
+
+  get email() {
+    return this.props.email;
+  }
+
+  get password() {
+    return this.props.password;
+  }
+
+  get createdAt(): Date | null {
+    return this.props.createdAt;
+  }
+
+  get updatedAt(): Date | null {
+    return this.props.updatedAt;
+  }
+
+  set name(name: string) {
+    this.props.name = name;
+    this.touch();
+  }
+
+  set email(email: string) {
+    this.props.email = email;
+    this.touch();
+  }
+
+  set password(password: string) {
+    this.props.password = password;
+    this.touch();
+  }
+
+  set createdAt(createdAt: Date) {
+    this.props.createdAt = createdAt;
+    this.touch();
+  }
+
+  set updatedAt(updatedAt: Date) {
+    this.props.updatedAt = updatedAt;
+    this.touch();
+  }
+
+  private touch() {
+    this.props.updatedAt = this.updatedAt;
+  }
+}
