ea9fe187d93bfe96a3b0f3ff1512563b93273f0d
Guilherme <guilhermeagenor1012@gmail.com>
2025-12-07 12:53:02 -0300

    feat: add prepration method entity

---
+++ src/core/entities/preparationMethod.ts
@@ -0,0 +1,76 @@
+import { UniqueEntityID } from "../domain/value-objects/unique-entity-id";
+
+export interface PreparationMethodProps {
+  step: string;
+  description: string;
+  recipeId: UniqueEntityID;
+  createdAt: Date | null;
+  createdBy: UniqueEntityID | null;
+  updatedAt: Date | null;
+  updatedBy: UniqueEntityID | null;
+  deletedAt: Date | null;
+  deletedBy: UniqueEntityID | null;
+}
+
+export class PreparationMethod {
+  constructor(
+    private readonly _id: UniqueEntityID,
+    private props: PreparationMethodProps,
+  ) {
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+      updatedAt: props.updatedAt ?? null,
+      deletedAt: props.deletedAt ?? null,
+      createdBy: props.createdBy ?? null,
+      updatedBy: props.updatedBy ?? null,
+      deletedBy: props.deletedBy ?? null,
+    };
+  }
+
+  get step() {
+    return this.props.step;
+  }
+
+  get description() {
+    return this.props.description;
+  }
+
+  get recipeId() {
+    return this.props.recipeId;
+  }
+
+  set createdAt(createdAt: Date) {
+    this.props.createdAt = createdAt;
+    this.touch();
+  }
+
+  set updatedAt(updatedAt: Date) {
+    this.props.updatedAt = updatedAt;
+    this.touch();
+  }
+
+  set deletedAt(deletedAt: Date) {
+    this.props.deletedAt = deletedAt;
+    this.touch();
+  }
+
+  set createdBy(createdBy: UniqueEntityID) {
+    this.props.createdBy = createdBy;
+    this.touch();
+  }
+
+  set updatedBy(updatedBy: UniqueEntityID) {
+    this.props.updatedBy = updatedBy;
+    this.touch();
+  }
+
+  set deletedBy(deletedBy: UniqueEntityID) {
+    this.props.deletedBy = deletedBy;
+    this.touch();
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date();
+  }
+}
