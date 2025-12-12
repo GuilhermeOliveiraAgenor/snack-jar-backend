be14093fd2f499ae025090a85f5c3b501231499d
Guilherme <guilhermeagenor1012@gmail.com>
2025-12-11 21:15:59 -0300

    feature: add either

--- src/application/use-cases/create-category.ts
+++ src/application/use-cases/create-category.ts
@@ -1,39 +1,42 @@
-  import { Category } from "../../core/entities/category";
-  import { categoriesRepository } from "../repositories/categories-repository";
-  import { AlreadyExistsError } from "../errors/already-exists-error";
+import { Category } from "../../core/entities/category";
+import { categoriesRepository } from "../repositories/categories-repository";
+import { AlreadyExistsError } from "../errors/already-exists-error";
 
-  interface CreateCategoryUseCaseRequest {
-    // create data request
-    name: Category["name"];
-    description: Category["description"];
-  }
+interface CreateCategoryUseCaseRequest {
+  // create data request
+  name: Category["name"];
+  description: Category["description"];
+}
 
-  interface CreateCategoryUseCaseResponse {
-    // create response
+// create response
+type CreateCategoryUseCaseResponse = Either<
+  AlreadyExistsError,
+  {
     category: Category;
   }
+>;
 
-  export class CreateCategoryUseCase {
-    constructor(private categoriesRepository: categoriesRepository) {} // define repository
+export class CreateCategoryUseCase {
+  constructor(private categoriesRepository: categoriesRepository) {} // define repository
 
-    async execute({
+  async execute({
+    name,
+    description,
+  }: CreateCategoryUseCaseRequest): Promise<CreateCategoryUseCaseResponse> {
+    // verify if category already exists
+    const categoryName = await this.categoriesRepository.findByName(name);
+    if (categoryName) {
+      throw new AlreadyExistsError("category");
+    }
+
+    // create object
+    const category = Category.create({
       name,
       description,
-    }: CreateCategoryUseCaseRequest): Promise<CreateCategoryUseCaseResponse> {
-      // verify if category already exists
-      const categoryName = await this.categoriesRepository.findByName(name);
-      if (categoryName) {
-        throw new AlreadyExistsError("category");
-      }
+    });
 
-      // create object
-      const category = Category.create({
-        name,
-        description,
-      });
+    await this.categoriesRepository.create(category); // pass to repository
 
-      await this.categoriesRepository.create(category); // pass to repository
-
-      return { category };
-    }
+    return { category };
   }
+}
