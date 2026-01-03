c69695aaf35d5a60711ff0924ffe6bea8f24ad5f
Guilherme <guilhermeagenor1012@gmail.com>
2026-01-03 16:11:24 -0300

    feature: add prisma user repository

---
+++ src/infra/repositories/PrismaUserRepository.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from "@prisma/client";
+import { UserRepository } from "../../application/repositories/user-repository";
+import { User } from "../../core/entities/user";
+import { PrismaUserMapper } from "../mappers/prisma-user-mapper";
+
+export class PrismaUserRepository implements UserRepository {
+  constructor(private readonly prisma: PrismaClient) {}
+  async create(user: User): Promise<void> {
+    await this.prisma.user.create({
+      data: PrismaUserMapper.toPersistency(user),
+    });
+  }
+  async findById(id: string): Promise<User | null> {
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+    });
+    if (!user) return null;
+    return PrismaUserMapper.toDomain(user);
+  }
+  async findByEmail(email: string): Promise<User | null> {
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+    });
+    if (!user) return null;
+    return PrismaUserMapper.toDomain(user);
+  }
+}
