842f11b31a75a72271b41a95bc141dc9b4622ce2
Guilherme Oliveira Agenor <guilherme.oliveira@superfestval.com.br>
2026-01-12 17:40:59 -0300

    feature: add factory controller

---
+++ src/http/factories/make-fetch-category-controller.ts
@@ -0,0 +1,21 @@
+import { FetchCategoriesUseCase } from "../../application/use-cases/category/fetch-categories"
+import { getPrismaClient } from "../../infra/prisma/client"
+import { PrismaCategoryRepository } from "../../infra/repositories/prisma-category-repository"
+import { FetchCategoryController } from "../controllers/category/fetch-category-controller"
+
+export function makeFetchCategoryController{
+    const prisma = getPrismaClient()
+
+    const categoryRepository = new PrismaCategoryRepository(prisma)
+    const fetchCategoryUseCase = new FetchCategoriesUseCase(categoryRepository)
+
+    return new FetchCategoryController(fetchCategoryUseCase)
+}
+
+
+
+
+
+
+
+
