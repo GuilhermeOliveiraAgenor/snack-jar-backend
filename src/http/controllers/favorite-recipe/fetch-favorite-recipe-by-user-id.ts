7b70f1d6501cc10576189d24482845585964e2cd
Guilherme Oliveira Agenor <guilherme.oliveira@superfestval.com.br>
2026-01-14 11:55:57 -0300

    feature: add fetch my favorite recipes

--- src/http/controllers/favorite-recipe/fetch-favorite-recipe-by-user-id.ts
+++ src/http/controllers/favorite-recipe/fetch-favorite-recipe-by-user-id.ts
@@ -1,3 +1,24 @@
+import { Request, Response, NextFunction } from "express";
+import { FetchMyRecipesUseCase } from "../../../application/use-cases/recipe/fetch-my-recipes";
 
-export class FetchRecipe
+export class FetchMyRecipesController{
+    constructor(private readonly fetchMyRecipesUseCase: FetchMyRecipesUseCase){}
+
+    async handle(req: Request,res: Response,next: NextFunction){
+        try {
+            const userId = req.user.id
+
+            const result = await this.fetchMyRecipesUseCase.execute({userId})
+
+            if(result.isError()){
+                throw result.value
+            }
+            
+            return res.status(200).json(result.value.recipe)
+
+        } catch (error) {
+            
+        }
+    }
+}
 
