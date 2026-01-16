e2eae09f88da781fecabd48fbe415f68a11d4dcb
Guilherme Oliveira Agenor <guilherme.oliveira@superfestval.com.br>
2026-01-16 10:14:54 -0300

    feature: add delete recipe controller

---
+++ src/http/controllers/recipe/delete-recipe-controller.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from "express";
+import { DeleteRecipeUseCase } from "../../../application/use-cases/recipe/delete-recipe";
+import z from "zod";
+
+const requestParams = z.object({
+    id: z.string()
+})
+
+export class DeleteRecipeController{
+    constructor( private readonly deleteRecipeUseCase: DeleteRecipeUseCase){}
+    
+    async handle(req: Request,res: Response,next: NextFunction){
+        try {
+            const userId = req.user.id
+            const { id } = requestParams.parse(req.params)
+            
+            const result = await this.deleteRecipeUseCase.execute({id, deletedBy: userId})
+    
+            if(result.isError()){
+                throw result.value
+            }
+
+            return res.status(200).json({message: "Recipe deleted successfully"})
+
+        } catch (error) {
+            next(error)
+        }
+    }
+
+}
