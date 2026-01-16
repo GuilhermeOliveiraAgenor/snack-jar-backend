82c50ed42965ed7fc02642b1224bdc61bc923a8c
Guilherme Oliveira Agenor <guilherme.oliveira@superfestval.com.br>
2026-01-16 10:39:32 -0300

    feature: add routes fetch my recipes

--- src/http/routes/recipe-route.ts
+++ src/http/routes/recipe-route.ts
@@ -1,6 +1,10 @@
 import { Router } from "express";
 import { makeCreateRecipeController } from "../factories/make-create-recipe-controller";
 import { makeAuthMiddleware } from "../factories/make-auth-middleware";
+import { makeEditRecipeController } from "../factories/make-edit-recipe-controller";
+import { makeDeleteRecipeController } from "../factories/make-delete-recipe-controller";
+import { FetchMyRecipesController } from "../controllers/recipe/fetch-my-recipes-controller";
+import { makeFetchMyRecipesController } from "../factories/make-fetch-my-recipe-controller";
 
 const recipeRoutes = Router();
 
@@ -9,5 +13,20 @@ recipeRoutes.use(makeAuthMiddleware());
 recipeRoutes.post("/recipe", (req, res, next) => {
   return makeCreateRecipeController().handle(req, res, next);
 });
+
+recipeRoutes.put("/recipe", (req,res,next) => {
+    return makeEditRecipeController().handle(req,res,next)
+})
+
+recipeRoutes.delete("/recipe", (req,res,next) =>{
+  return makeDeleteRecipeController().handle(req,res,next)
+})
+
+recipeRoutes.get("/menu", (req,res,next) => {
+  return makeFetchMyRecipesController().handle(req,res,next)
+})
+
+
+recipeRoutes
 
 export { recipeRoutes };
