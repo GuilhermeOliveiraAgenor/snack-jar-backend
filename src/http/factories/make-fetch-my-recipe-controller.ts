bb5495c12c33ae43cfecb0449ff925ff0a810a8c
Guilherme Oliveira Agenor <guilherme.oliveira@superfestval.com.br>
2026-01-16 10:31:52 -0300

    feature: add make fetch recipe by title

--- src/http/factories/make-fetch-my-recipe-controller.ts
+++ src/http/factories/make-fetch-my-recipe-controller.ts
@@ -1,13 +1,16 @@
 import { FetchMyRecipesUseCase } from "../../application/use-cases/recipe/fetch-my-recipes";
 import { getPrismaClient } from "../../infra/prisma/client";
 import { PrismaRecipeRepository } from "../../infra/repositories/prisma-recipe-repository";
+import { PrismaUserRepository } from "../../infra/repositories/prisma-user-repository";
 import { FetchMyRecipesController } from "../controllers/recipe/fetch-my-recipes-controller";
 
 export function makeFetchMyRecipesController(){
     const prisma = getPrismaClient()
 
     const recipeRepository = new PrismaRecipeRepository(prisma)
-    const fetchMyRecipesUseCase = new FetchMyRecipesUseCase(recipeRepository)
+    const userRepository = new PrismaUserRepository(prisma)
+
+    const fetchMyRecipesUseCase = new FetchMyRecipesUseCase(recipeRepository,userRepository)
 
     return new FetchMyRecipesController(fetchMyRecipesUseCase)
 
---
+++ src/http/factories/make-fetch-recipe-by-title-controller.ts
@@ -0,0 +1,16 @@
+import { FetchRecipeByTitleUseCase } from "../../application/use-cases/recipe/fetch-recipe-by-title";
+import { getPrismaClient } from "../../infra/prisma/client";
+import { PrismaRecipeRepository } from "../../infra/repositories/prisma-recipe-repository";
+import { PrismaUserRepository } from "../../infra/repositories/prisma-user-repository";
+import { FetchRecipeByTitleController } from "../controllers/recipe/fetch-recipe-by-title-controller";
+
+export function makeFetchRecipeByTitleController(){
+    const prisma = getPrismaClient()
+
+    const recipeRepository = new PrismaRecipeRepository(prisma)
+    const userRepository = new PrismaUserRepository(prisma)
+    const fetchRecipeByTitleUseCase = new FetchRecipeByTitleUseCase(recipeRepository, userRepository)
+
+    return new FetchRecipeByTitleController(fetchRecipeByTitleUseCase)
+
+}
