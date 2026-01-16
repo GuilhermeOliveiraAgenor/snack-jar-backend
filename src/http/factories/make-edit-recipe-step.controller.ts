e94f5ba7c90582bd6c92efb41bce1510893c530d
Guilherme Oliveira Agenor <guilherme.oliveira@superfestval.com.br>
2026-01-16 11:22:15 -0300

    feature: add edit recipe step ingredient factory

---
+++ src/http/factories/make-edit-recipe-step.controller.ts
@@ -0,0 +1,16 @@
+import { EditRecipeStepUseCase } from "../../application/use-cases/recipe-step/edit-recipe-step";
+import { getPrismaClient } from "../../infra/prisma/client";
+import { PrismaRecipeRepository } from "../../infra/repositories/prisma-recipe-repository";
+import { PrismaRecipeStepRepository } from "../../infra/repositories/prisma-recipe-step-repository";
+import { EditRecipeStepController } from "../controllers/recipe-step/edit-recipe-step.controller";
+
+export function makeEditRecipeStepController() {
+  const prisma = getPrismaClient();
+
+  const recipeStepRepository = new PrismaRecipeStepRepository(prisma);
+  const recipeRepository = new PrismaRecipeRepository(prisma);
+
+  const editRecipeStepUseCase = new EditRecipeStepUseCase(recipeStepRepository );
+
+  return new EditRecipeStepController(editRecipeStepUseCase);
+}
