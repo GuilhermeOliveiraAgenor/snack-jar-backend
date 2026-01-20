9a109ab956e1054e55e2514e85507517cea89f8c
Guilherme Oliveira Agenor <guilherme.oliveira@superfestval.com.br>
2026-01-20 17:08:19 -0300

    refactor: adjust factory with repositories

--- src/http/factories/make-delete-recipe-ingredient.controller.ts
+++ src/http/factories/make-delete-recipe-ingredient.controller.ts
@@ -1,14 +1,17 @@
 import { DeleteRecipeIngredientUseCase } from "../../application/use-cases/recipe-ingredient/delete-recipe-ingredient";
 import { getPrismaClient } from "../../infra/prisma/client";
 import { PrismaRecipeIngredientRepository } from "../../infra/repositories/prisma-recipe-ingredient-repository";
+import { PrismaRecipeRepository } from "../../infra/repositories/prisma-recipe-repository";
 import { DeleteRecipeIngredientController } from "../controllers/recipe-ingredient/delete-recipe-ingredient.controller";
 
 export function makeDeleteRecipeIngredientController() {
   const prisma = getPrismaClient();
 
   const recipeIngredientRepository = new PrismaRecipeIngredientRepository(prisma);
+  const recipeRepository = new PrismaRecipeRepository(prisma);
+
   const deleteRecipeIngredientUseCase = new DeleteRecipeIngredientUseCase(
-    recipeIngredientRepository,
+    recipeIngredientRepository,recipeRepository
   );
 
   return new DeleteRecipeIngredientController(deleteRecipeIngredientUseCase);
--- src/http/factories/make-delete-recipe-step-ingredient.controller.ts
+++ src/http/factories/make-delete-recipe-step-ingredient.controller.ts
@@ -1,5 +1,6 @@
 import { DeleteRecipeStepUseCase } from "../../application/use-cases/recipe-step/delete-recipe-step";
 import { getPrismaClient } from "../../infra/prisma/client";
+import { PrismaRecipeRepository } from "../../infra/repositories/prisma-recipe-repository";
 import { PrismaRecipeStepRepository } from "../../infra/repositories/prisma-recipe-step-repository";
 import { DeleteRecipeStepController } from "../controllers/recipe-step/delete-recipe-step.controller";
 
@@ -7,7 +8,9 @@ export function makeDeleteRecipeStepController() {
   const prisma = getPrismaClient();
 
   const recipeStepRepository = new PrismaRecipeStepRepository(prisma);
-  const deleteRecipeStepUseCase = new DeleteRecipeStepUseCase(recipeStepRepository);
+  const recipeRepository = new PrismaRecipeRepository(prisma);
+
+  const deleteRecipeStepUseCase = new DeleteRecipeStepUseCase(recipeStepRepository, recipeRepository);
 
   return new DeleteRecipeStepController(deleteRecipeStepUseCase);
 }
