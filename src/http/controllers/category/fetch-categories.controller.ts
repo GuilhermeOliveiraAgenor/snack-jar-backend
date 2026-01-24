de0c20604b68d81b67a895b80f36b7acd66d92e8
Guilherme <guilhermeagenor1012@gmail.com>
2026-01-24 10:34:00 -0300

    feature: add query in route fetch controller

--- src/http/controllers/category/fetch-categories.controller.ts
+++ src/http/controllers/category/fetch-categories.controller.ts
@@ -1,13 +1,20 @@
 import { Request, Response, NextFunction } from "express";
 import { FetchCategoriesUseCase } from "../../../application/use-cases/category/fetch-categories";
 import { CategoryPresenter } from "../../presenters/category-presenter";
+import z from "zod";
+
+export const fetchCategoriesParamsSchema = z.object({
+  page: z.coerce.number().int().min(1).default(1),
+});
 
 export class FetchCategoriesController {
   constructor(private readonly fetchCategoriesUseCase: FetchCategoriesUseCase) {}
 
   async handle(req: Request, res: Response, next: NextFunction) {
     try {
-      const result = await this.fetchCategoriesUseCase.execute({});
+      const { page } = fetchCategoriesParamsSchema.parse(req.query);
+
+      const result = await this.fetchCategoriesUseCase.execute({ page });
 
       if (result.isError()) {
         throw result.value;
--- src/http/presenters/pagination.meta.ts
+++ src/http/presenters/pagination.meta.ts
@@ -2,5 +2,5 @@ export interface PaginationMeta {
   page: number;
   per_page: number;
   total_count: number;
-  filters?: Record<string, any>;
+  filters?: Record<string, unknown>;
 }
